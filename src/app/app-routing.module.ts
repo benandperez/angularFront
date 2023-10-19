30b9e8ca2217d2c2c86653f40f23c3147bb5bdda
benandperez <creoencristo7@hotmail.com>
2023-10-19 01:17:51 -0500

    subiendo faltando solo icon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorMessagesComponent } from './components/error-messages/error-messages.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { ScoreGuard } from './score.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/user-list', pathMatch: 'full' },
+  { path: 'user-list', component: UserListComponent },
+  { path: 'error', component: ErrorMessagesComponent },
+  {
+    path: 'user-profile',
+    component: UserProfileComponent,
+    canActivate: [ScoreGuard],
+  },
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import {NgbAlertModule, NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { NgChartsModule } from 'ng2-charts';
+import { ErrorMessagesComponent } from 'src/app/components/error-messages/error-messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserProfileComponent,
+    ErrorMessagesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+    CanvasJSAngularChartsModule,
+    NgChartsModule,
+    NgbAlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
